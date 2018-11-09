7b8d2e99e9ebe4b9d47f8429a81bc3f13744ca78
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-11-09 17:16:54 +0800

    Header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>Home</div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-left">&#xe60c;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题</div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe6aa;</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    line-height: .86rem
+    background #00bcd4
+    color #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-left
+        text-align center
+        font-size .2rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align center
+    .header-input
+      flex: 1
+      height .64rem
+      border-radius .1rem
+      margin-top .12rem
+      margin-left .2rem
+      padding-left .2rem
+      line-height .64rem
+      background #fff
+      color #ccc
+</style>
