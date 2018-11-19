a22d00bc735ba1a927107fa0078e02ec2f1cbf21
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-11-19 19:01:32 +0800

    better-scroll

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,19 +2,21 @@
     <div>
       <city-header></city-header>
       <city-search></city-search>
+      <city-list></city-list>
     </div>
 </template>
 
 <script>
   import CityHeader from './components/Header'
   import CitySearch from './components/Search'
+  import CityList from './components/List'
     export default {
         name: "city",
       components:{
         CityHeader,
-        CitySearch
+        CitySearch,
+        CityList
       }
-
     }
 </script>
 
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+
+          </div>
+
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from "better-scroll"
+
+  export default {
+    name: "list",
+    mounted() {
+
+      // this.scroll = new BScroll(this.$refs.wrapper)
+      let wrapper = document.querySelector('.list')
+      let scroll = new BScroll(wrapper)
+
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@/assets/styles/varibles.styl"
+  .border-topbottom
+    &:befroe
+      border-color: #777
+    &:after
+      border-color: #777
+
+  .border-bottom
+  &:befroe
+    border-color: #777
+
+  .list
+    overflow hidden
+    position absolute
+    top 1.68rem
+    left 0
+    right 0
+    bottom 0
+    .title
+      line-height .54rem
+      background #eee
+      color #666
+      padding-left .2rem
+      font-size .26rem
+    .button-list
+      padding .1rem .6rem .1rem .1rem
+      overflow hidden
+      .button-wrapper
+        float left
+        width 33.33%
+        .button
+          text-align center
+          margin .1rem
+          border .02rem solid #ccc
+          padding .1rem 0
+          border-radius .06rem
+    .item-list
+      .item
+        line-height .54rem
+        padding-left .2rem
+        color #666
+</style>
